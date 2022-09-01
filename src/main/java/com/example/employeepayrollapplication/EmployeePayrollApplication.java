7c585873c0149592887b77108d3de32ab03e28b3
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-09-01 17:10:12 +0530

    Modified: Dev propertise created so to show log info properly

--- src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
@@ -1,14 +1,18 @@
 package com.example.employeepayrollapplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee payroll Application");
         SpringApplication.run(EmployeePayrollApplication.class, args);
+        System.out.println("Welcome to the Employee payroll Application");
+        System.out.println("--------------------------------------------");
+        log.info("Hello dear logger........!");
     }
 
 }
